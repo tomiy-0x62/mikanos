12ff241a253061bf42f82dc4777eff2a58b7cbd1
Ryosuke <ryosuke1538>
2021-11-05 15:50:18 +0900

    generate gdb_init dynamicly

---
+++ gdb.sh
@@ -0,0 +1,28 @@
+rm -rf ./gdb_init
+
+echo "# qemuに接続
+target remote localhost:1234
+
+file ./kernel/kernel.elf
+
+# 逆アセンブル結果をintel形式に
+set disassembly-flavor intel
+
+# ブレークするごとに逆アセンブル結果を表示
+disp/5i \$pc
+" >> ./gdb_init
+
+
+echo "# IntHandlerPF" >> ./gdb_init
+nm -C ./kernel/kernel.elf | grep IntHandlerPF | awk '{printf "b *0x%s", $1}' >> ./gdb_init
+echo "" >> ./gdb_init
+
+echo "# IntHandlerGP" >> ./gdb_init
+nm -C ./kernel/kernel.elf | grep IntHandlerGP | awk '{printf "b *0x%s", $1}' >> ./gdb_init
+echo "" >> ./gdb_init
+
+echo "# IntHandlerUD" >> ./gdb_init
+nm -C ./kernel/kernel.elf | grep IntHandlerUD | awk '{printf "b *0x%s", $1}' >> ./gdb_init
+echo "" >> ./gdb_init
+
+gdb -x gdb_init
